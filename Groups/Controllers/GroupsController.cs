d0d037cca8aebcf30f89c2c8ee241bc4f249a8de
phikko <skeezze@gmail.com>
2022-01-08 20:53:01 +0100

    added group single get and edit

--- Groups/Controllers/GroupsController.cs
+++ Groups/Controllers/GroupsController.cs
@@ -12,6 +12,20 @@ public class GroupsController : ControllerBase
     {
         return new GroupDbEntities().Groups.ToList();
     }
+    
+    [HttpGet("{id}", Name = "GetGroup")]
+    public ObjectResult Get(int id)
+    {
+        var group = new GroupDbEntities().Groups.Find(id);
+        if (group == null)
+        {
+            return NotFound(group);
+        }
+        else
+        {
+            return Ok(group);
+        }
+    }
 
     [HttpPost(Name = "CreateGroup")]
     public Group Post(Group group)
@@ -20,5 +34,21 @@ public class GroupsController : ControllerBase
         var groupEntity = groupDb.Add(group);
         groupDb.SaveChanges();
         return groupEntity.Entity;
+    }
+
+    [HttpPut("{id}", Name = "GetGroup")]
+    public ObjectResult Put(int id, Group group)
+    {
+        var groupDb = new GroupDbEntities();
+        var currentGroup = groupDb.Groups.Find(id);
+        if (currentGroup == null)
+        {
+            return NotFound(currentGroup);
+        }
+
+        currentGroup.Name = group.Name;
+        currentGroup.Budget = group.Budget;
+        groupDb.SaveChanges();
+        return Ok(currentGroup);
     }
 }
