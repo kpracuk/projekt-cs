871b16b30a48f7250d257087169297fc6bdbe266
phikko <skeezze@gmail.com>
2022-01-09 20:46:33 +0100

    added api client

---
+++ ConsoleUI/Api/Client.cs
@@ -0,0 +1,10 @@
+﻿using ConsoleUI.Api.Modules;
+
+namespace ConsoleUI.Api;
+
+public class Client
+{
+    static readonly HttpClient client = new HttpClient();
+    
+    public Groups Groups = new Groups(client);
+}
---
+++ ConsoleUI/Classes/Group.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleUI.Classes;
+
+public class Group
+{
+    public int Id { get; set; }
+    
+    public string Name { get; set; } = "";
+
+    public int Budget { get; set; }
+}
