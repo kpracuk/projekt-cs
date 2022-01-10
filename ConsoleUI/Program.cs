86a39af6ecba04e9f65a0c887855fd4be210a067
phikko <skeezze@gmail.com>
2022-01-10 20:58:44 +0100

    added group editing

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,38 +1,7 @@
 ﻿using Terminal.Gui;
-using ConsoleUI.Views.Groups;
+using ConsoleUI.Views;
 
 Application.Init();
-
-var content = new View {
-    X = 0,
-    Y = 1,
-    Height = Dim.Fill(),
-    Width = Dim.Fill()
-};
-
-var menu = new MenuBar(new[] {
-    new MenuBarItem ("Projekty", new[] {
-        new MenuItem ("Lista", "", async () =>
-        {
-            var view = await new ListGroups().Render();
-            RenderInContent(view);
-        }),
-        new MenuItem ("Dodaj", "", async () =>
-        {
-            var view = await new CreateGroup().Render();
-            RenderInContent(view);
-        })
-    })
-});
-
-void RenderInContent(View view) {
-    content.RemoveAll();
-    content.Add(view);
-}
-
-var app = Application.Top;
-
-app.Add(menu);
-app.Add(content);
-
+var main = Main.GetInstance();
+main.Run();
 Application.Run();
